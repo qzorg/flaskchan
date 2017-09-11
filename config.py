9fee20f643b5b080efb335e03b2d7ab805d8cfbe
qzo <qzo@waifu.club>
2017-09-11 14:10:51 -0600

    Added boards to config
    now all the config stuff should work correctly out of the box in the dev
    environment. The server environment still needs to be handled manually
    though, so please make sure to run the update script instead of doing a
    git pull.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 BUMP_LIMIT         = 100
-BOARDS             = ['create','learn','media','meta', 'programming', 'tech', 'lewd']
+BOARDS             = ['all', 'diy', 'lam', 'r', 'meta', 'cult', 'create','learn','media','meta', 'programming', 'tech', 'lewd']
 UPLOAD_FOLDER      = 'static/images/'
 THUMBS_FOLDER      = 'static/thumbs/'
 ALLOWED_EXTENSIONS =  set(['png','jpg','jpeg','gif', 'css', 'webm'])
